d92c9c50a5da539bb82f6b6c42cacbf91acf4e61
Georgy <thesuperboy112@gmail.com>
2021-02-14 23:47:54 +0300

    Done HomeWork

---
+++ Домашняя работа 14.02.2021/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace Домашняя_работа_14._02._2021
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.ThirdNameBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.GenderManCheck = new System.Windows.Forms.CheckBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.WomanGenderCheck = new System.Windows.Forms.CheckBox();
+            this.SurnameBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.MaritalStatusYesCheck = new System.Windows.Forms.CheckBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.MaritalStatusNoCheck = new System.Windows.Forms.CheckBox();
+            this.AnotherInfoBox = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.DateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.DataBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Отчество";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Пол";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Год и Дата Рождения";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(128, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "СП";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(128, 334);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Доп. информация";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(245, 56);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(242, 23);
+            this.NameBox.TabIndex = 7;
+            // 
+            // ThirdNameBox
+            // 
+            this.ThirdNameBox.Location = new System.Drawing.Point(245, 135);
+            this.ThirdNameBox.Name = "ThirdNameBox";
+            this.ThirdNameBox.Size = new System.Drawing.Size(242, 23);
+            this.ThirdNameBox.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(353, 190);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 15);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "М";
+            // 
+            // GenderManCheck
+            // 
+            this.GenderManCheck.AutoSize = true;
+            this.GenderManCheck.Location = new System.Drawing.Point(377, 191);
+            this.GenderManCheck.Name = "GenderManCheck";
+            this.GenderManCheck.Size = new System.Drawing.Size(15, 14);
+            this.GenderManCheck.TabIndex = 11;
+            this.GenderManCheck.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(448, 191);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 15);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Ж";
+            // 
+            // WomanGenderCheck
+            // 
+            this.WomanGenderCheck.AutoSize = true;
+            this.WomanGenderCheck.Location = new System.Drawing.Point(472, 192);
+            this.WomanGenderCheck.Name = "WomanGenderCheck";
+            this.WomanGenderCheck.Size = new System.Drawing.Size(15, 14);
+            this.WomanGenderCheck.TabIndex = 13;
+            this.WomanGenderCheck.UseVisualStyleBackColor = true;
+            // 
+            // SurnameBox
+            // 
+            this.SurnameBox.Location = new System.Drawing.Point(245, 94);
+            this.SurnameBox.Name = "SurnameBox";
+            this.SurnameBox.Size = new System.Drawing.Size(242, 23);
+            this.SurnameBox.TabIndex = 14;
+            this.SurnameBox.TextChanged += new System.EventHandler(this.SurnameBox_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(190, 283);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 15);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Женат/Замужем";
+            // 
+            // MaritalStatusYesCheck
+            // 
+            this.MaritalStatusYesCheck.AutoSize = true;
+            this.MaritalStatusYesCheck.Location = new System.Drawing.Point(295, 284);
+            this.MaritalStatusYesCheck.Name = "MaritalStatusYesCheck";
+            this.MaritalStatusYesCheck.Size = new System.Drawing.Size(15, 14);
+            this.MaritalStatusYesCheck.TabIndex = 16;
+            this.MaritalStatusYesCheck.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(347, 282);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(119, 15);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Холост/Не замужем";
+            // 
+            // MaritalStatusNoCheck
+            // 
+            this.MaritalStatusNoCheck.AutoSize = true;
+            this.MaritalStatusNoCheck.Location = new System.Drawing.Point(472, 283);
+            this.MaritalStatusNoCheck.Name = "MaritalStatusNoCheck";
+            this.MaritalStatusNoCheck.Size = new System.Drawing.Size(15, 14);
+            this.MaritalStatusNoCheck.TabIndex = 18;
+            this.MaritalStatusNoCheck.UseVisualStyleBackColor = true;
+            // 
+            // AnotherInfoBox
+            // 
+            this.AnotherInfoBox.Location = new System.Drawing.Point(270, 331);
+            this.AnotherInfoBox.Name = "AnotherInfoBox";
+            this.AnotherInfoBox.Size = new System.Drawing.Size(217, 23);
+            this.AnotherInfoBox.TabIndex = 19;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(273, 385);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(214, 26);
+            this.SaveButton.TabIndex = 20;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DateTimePicker
+            // 
+            this.DateTimePicker.Location = new System.Drawing.Point(287, 219);
+            this.DateTimePicker.Name = "DateTimePicker";
+            this.DateTimePicker.Size = new System.Drawing.Size(200, 23);
+            this.DateTimePicker.TabIndex = 21;
+            // 
+            // DataBox
+            // 
+            this.DataBox.Location = new System.Drawing.Point(245, 12);
+            this.DataBox.Name = "DataBox";
+            this.DataBox.Size = new System.Drawing.Size(231, 23);
+            this.DataBox.TabIndex = 8;
+            this.DataBox.Text = "ГГГГ.ММ.ДД";
+            this.DataBox.Visible = false;
+            this.DataBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DataBox_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.DateTimePicker);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.AnotherInfoBox);
+            this.Controls.Add(this.MaritalStatusNoCheck);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.MaritalStatusYesCheck);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.SurnameBox);
+            this.Controls.Add(this.WomanGenderCheck);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.GenderManCheck);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.ThirdNameBox);
+            this.Controls.Add(this.DataBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Информация о человеке";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox ThirdNameBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox GenderManCheck;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox WomanGenderCheck;
+        private System.Windows.Forms.TextBox SurnameBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckBox MaritalStatusYesCheck;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.CheckBox MaritalStatusNoCheck;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox AnotherInfoBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.DateTimePicker DateTimePicker;
+        private System.Windows.Forms.TextBox DataBox;
+    }
+}
+
---
+++ Домашняя работа 14.02.2021/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Домашняя_работа_14._02._2021
+{
+    public partial class Form1 : Form
+    {
+        
+        private bool choice = new bool();
+        private string[] info_massive = new string[] { };
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void SurnameBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            choice = true;
+            Information();
+            
+            if (choice == true && Check(info_massive, true) == true)
+            {
+                SaveFileDialog file = new SaveFileDialog();
+                file.Filter = "txt files (*.txt)|*.txt";
+                file.FilterIndex = 2;
+                file.RestoreDirectory = true;
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    TextWriter tw = new StreamWriter(file.OpenFile());
+                    if (tw != null)
+                    {
+                        foreach (String item in Information())
+                            tw.WriteLine(item);
+                        tw.Close();
+                    }
+                }
+            }
+        }
+
+        private bool ErrorMessage(bool a, string message, string labeltext) 
+        {
+
+            string caption = "Ошибка в заполнении поля "+labeltext;
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result;
+            result = MessageBox.Show(message, caption, buttons);
+            a = false;
+            return a;
+        }
+
+        private string SomeChoice(string choice1, string choice2, CheckBox Box1, CheckBox Box2, string labelname)
+        {
+            
+            if (Box1.Checked)
+            {
+                return choice1;
+            }
+            else if (Box2.Checked)
+            {
+                return choice2;
+            }
+            else if (Box1.Checked && Box2.Checked)
+            {
+                ErrorMessage(choice, "Выберите одно значение", labelname);
+                return "";
+            }
+            else
+            {
+                ErrorMessage(choice, "Выберите одно значение", labelname);
+                return "";
+            }
+
+        }
+        private bool Check(string[] mass, bool a)
+        {
+            
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] == "")
+                    a = false;
+            }
+            return a;
+        }
+        private List<String> Information()
+        {   
+            string name = NameBox.Text;
+            string surname = SurnameBox.Text;
+            string thirdname = ThirdNameBox.Text;
+            string gender_choice = SomeChoice("мужчина", "женщина", GenderManCheck, WomanGenderCheck, label4.Text);
+            DateTime dateboxtext = DateTimePicker.Value;
+            string data = dateboxtext.ToString("D");
+            string marital_choice = "";
+            if (GenderManCheck.Checked == true && WomanGenderCheck.Checked == false)
+                marital_choice = SomeChoice("женат", "холост", MaritalStatusYesCheck, MaritalStatusNoCheck, label6.Text);
+            else if (WomanGenderCheck.Checked == true && GenderManCheck.Checked == false)
+            {
+                marital_choice = SomeChoice("замужем", "не замужем", MaritalStatusYesCheck, MaritalStatusNoCheck, label6.Text);
+            }
+            else
+            {
+                ErrorMessage(choice, "Выберите одно значение", "Для любого поля");
+            }
+
+            string another_information = AnotherInfoBox.Text;
+            
+            info_massive = new string[]
+            {
+                name, surname, thirdname, gender_choice, data, marital_choice, another_information
+            };
+            
+            //Check(info_massive, true);
+            List<string> Info = new List<string>(info_massive);
+
+            return Info;
+        }
+
+        private void DataBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            DataBox.Text = "";
+        }
+    }
+}
